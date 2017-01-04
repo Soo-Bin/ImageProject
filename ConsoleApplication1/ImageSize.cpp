5a7a0c44fb050de57facb0f914b9fe01e51351ab
Soo-Bin <weeiqp@gmail.com>
2017-01-04 11:13:36 +0900

    Updated ImageSize.cpp

--- ConsoleApplication1/ImageSize.cpp
+++ ConsoleApplication1/ImageSize.cpp
@@ -7,70 +7,16 @@
 #include <opencv2/core/core.hpp>   
 #include <opencv2/highgui/highgui.hpp>  
 
-#define SIZE 1.3
-
 using namespace cv;
 
-#define PI 3.14159265359
+#define SIZE 1.3
 
-int** IntAlloc2(int width, int height)
-{
-	int** tmp;
-	tmp = (int**)calloc(height, sizeof(int*));
-	for (int i = 0; i<height; i++)
-		tmp[i] = (int*)calloc(width, sizeof(int));
-	return(tmp);
-}
-
-void IntFree2(int** image, int width, int height)
-{
-	for (int i = 0; i<height; i++)
-		free(image[i]);
-
-	free(image);
-}
-
-int** ReadImage(char* name, int* width, int* height)
-{
-	Mat img = imread(name, IMREAD_GRAYSCALE);
-	int** image = (int**)IntAlloc2(img.cols, img.rows);
-
-	*width = img.cols;
-	*height = img.rows;
-
-	for (int i = 0; i<img.rows; i++)
-		for (int j = 0; j<img.cols; j++)
-			image[i][j] = img.at<unsigned char>(i, j);
-
-	return(image);
-}
-
-void WriteImage(char* name, int** image, int width, int height)
-{
-	Mat img(height, width, CV_8UC1);
-	for (int i = 0; i<height; i++)
-		for (int j = 0; j<width; j++)
-			img.at<unsigned char>(i, j) = (unsigned char)image[i][j];
-
-	imwrite(name, img);
-}
-
-void ImageShow(char* winname, int** image, int width, int height)
-{
-	Mat img(height, width, CV_8UC1);
-	for (int i = 0; i<height; i++)
-		for (int j = 0; j<width; j++)
-			img.at<unsigned char>(i, j) = (unsigned char)image[i][j];
-	imshow(winname, img);
-
-}
-
-void CopyImage(int** src, int width, int height, int** dst)
-{
-	for (int y = 0; y<height; y++)
-		for (int x = 0; x<width; x++)
-			dst[y][x] = src[y][x];
-}
+int** IntAlloc2(int width, int height);
+void IntFree2(int** image, int width, int height);
+int** ReadImage(char* name, int* width, int* height);
+void WriteImage(char* name, int** image, int width, int height);
+void ImageShow(char* winname, int** image, int width, int height);
+void CopyImage(int** src, int width, int height, int** dst);
 
 int Bilinear_Interp(int** image, int height, int width, double y, double x)
 {
@@ -107,7 +53,7 @@ void Interpolation(int** image, int width, int height, int** image_out)
 		}
 }
 
-void main()
+void main_interpolation()
 {
 	int width, height;
 	int** image = ReadImage("Hydrangeas.jpg", &width, &height);
@@ -125,5 +71,3 @@ void main()
 	IntFree2(image_out, width*SIZE, height*SIZE);
 
 }
-
-
