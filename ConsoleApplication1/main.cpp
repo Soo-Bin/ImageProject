4273632e7e9bd787a3a0bc4e3b575c833f1a7750
Soo-Bin <weeiqp@gmail.com>
2017-01-04 11:30:18 +0900

    void Main()

---
+++ ConsoleApplication1/main.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <malloc.h>
+
+#include <opencv2/opencv.hpp>   
+#include <opencv2/core/core.hpp>   
+#include <opencv2/highgui/highgui.hpp>  
+
+void main_CircleImage();
+void main_AvgFiltering();
+void main_interpolation();
+void main_rotaion();
+int main_LifeCam();
+
+void main()
+{
+	main_CircleImage();
+	main_AvgFiltering();
+	main_interpolation();
+	main_rotaion();
+	main_LifeCam();
+}
+
